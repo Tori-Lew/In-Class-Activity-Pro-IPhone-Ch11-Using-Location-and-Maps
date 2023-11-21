760310789674871595b2ea81143e62b326edfd05
Tori Lew <55900873+Tori-Lew@users.noreply.github.com>
2023-11-20 16:27:01 -0800

    Initial commit

---
+++ In-Class Activity Pro IPhone Ch11 Using Location and Maps/In_Class_Activity_Pro_IPhone_Ch11_Using_Location_and_MapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  In_Class_Activity_Pro_IPhone_Ch11_Using_Location_and_MapsApp.swift
+//  In-Class Activity Pro IPhone Ch11 Using Location and Maps
+//
+//  Created by Student Account on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct In_Class_Activity_Pro_IPhone_Ch11_Using_Location_and_MapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
