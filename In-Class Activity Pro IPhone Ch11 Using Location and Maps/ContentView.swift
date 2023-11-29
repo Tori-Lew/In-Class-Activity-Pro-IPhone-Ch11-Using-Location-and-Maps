dba7fca3e7819980558b1601df6a5e6e4f5bbdd3
Tori Lew <torirlew@gmail.com>
2023-11-28 17:09:33 -0800

    There is a map now

--- In-Class Activity Pro IPhone Ch11 Using Location and Maps/ContentView.swift
+++ In-Class Activity Pro IPhone Ch11 Using Location and Maps/ContentView.swift
@@ -6,16 +6,41 @@
 //
 
 import SwiftUI
+import MapKit
+
+extension CLLocationCoordinate2D {
+    static let cascadiaCollege = CLLocationCoordinate2D(latitude: 47.76096766707533, longitude:-122.19158954788811)
+    static let bothellHigh = CLLocationCoordinate2D(latitude: 47.76096297259954, longitude:-122.21924592963471)
+    static let blythePark = CLLocationCoordinate2D(latitude: 47.75027775753351, longitude:-122.2088547318621)
+}
 
 struct ContentView: View {
+    let locationManager = CLLocationManager()
+    @State var message = "Map of Bothell"
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            Map(){
+                Marker("Bothell High School", coordinate: .bothellHigh)
+                Annotation("Blythe Park", coordinate: .blythePark){
+                    Image(systemName: "mappin")
+                        .foregroundColor(.blue)
+                }
+                Annotation("Cascadia College", coordinate: .cascadiaCollege){
+                    Image(systemName: "graduationcap.fill")
+                        .foregroundColor(.black)
+                }
+            }
+            .frame(width: 300, height: 500)
+            TextEditor(text: $message)
+                .frame(width: .infinity, height: 100)
         }
         .padding()
+        .onAppear {
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.distanceFilter = kCLDistanceFilterNone
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
+        }
     }
 }
 
